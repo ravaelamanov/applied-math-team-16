8d63c58133004046a64f6dfe3f4c599863ea1e6a
ravaelamanov <ravaelamanov@gmail.com>
2021-03-08 00:49:04 +0300

    deleted function.py

--- lab1/function.py
+++
@@ -1,9 +0,0 @@
-class Function:
-    def __init__(self, f, a, b, x0):
-        self.f = f
-        self.a = a
-        self.b = b
-        self.x0 = x0
-    
-    def params(self):
-        return (self.f, self.a, self.b)
