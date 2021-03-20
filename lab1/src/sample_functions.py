5598f0b08276661c83c73e2a2137bd49a2f26e7a
Ravshan Elamanov <ravaelamanov@gmail.com>
2021-03-20 17:16:42 +0300

    Added two more multimodal functions

--- lab1/src/sample_functions.py
+++ lab1/src/sample_functions.py
@@ -11,3 +11,11 @@ def func_1(x):
 
 def multimodal1(x):
     return x ** 4 - 8 * x ** 3 + 22 * x ** 2 - 24 * x + 1
+
+
+def multimodal2(x):
+    return math.sin(x) + math.cos(math.sqrt(2) * x) + math.sin(math.sqrt(3) * x)
+
+
+def multimodal3(x):
+    return 2 + math.cos(x) + math.cos(2 * x - 1 / 2) / 2
